70bc83059695459ace4efd9f1e956a0706e2e1ea
yangtong <bitsoal@gmail.com>
2018-04-02 21:04:16 +0800

    a series of modifications
    1. update fig "The preocedure of preparing VASP input files and relevant tags"
    2. Create Execute_user_defined_cmd.py
    3.  Add codes to new_Preprocess_and_Postprocess in order to take actions for new tags user_defined_cmd, final_user_defined_cmd, user_defined_post_process_cmd
    4.  enable Parse_calculation_workflow.py to parse user_defined_cmd, final_user_defined_cmd, user_defined_post_process_cmd
    5.  Utilities.py: create wrapped os.system, namely, decoreated_os_system
    6. Write_VASP_POSCAR.py:  change the meaning of tag sort_structure. If sort_structure is False and the given structure is not POSCAR-formated, use pymatgen.Structure to write POSCAR. If sort_structure is False and the given structure is POSCAR-formated, just copy that file and rename it as POSCAR
    7. htc_main.py: fix the bug that the program stops before all calculations are done
    8. update manual

--- htc_main.py
+++ htc_main.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[1]:
 
 
 import os, sys, time, pprint
@@ -20,7 +20,7 @@ if  os.path.join(HTC_lib_path, "HTC_lib") not in sys.path:
 
 from HTC_lib.Utilities import get_time_str
 from HTC_lib.Parse_calculation_workflow import parse_calculation_workflow
-from HTC_lib.Preprocess_and_Postprocess import pre_and_post_process
+from HTC_lib.new_Preprocess_and_Postprocess import pre_and_post_process
 from HTC_lib.Check_and_update_calculation_status import check_calculations_status
 from HTC_lib.Check_and_update_calculation_status import update_running_jobs_status
 from HTC_lib.Check_and_update_calculation_status import update_killed_jobs_status
@@ -37,8 +37,7 @@ if __name__ == "__main__":
     while True:
         cif_file_list = os.listdir(cif_file_folder)
         for cif_file in cif_file_list:
-            cif_file_path = os.path.join(cif_file_folder, cif_file)
-            pre_and_post_process(cif_filename=cif_file_path, cal_folder=cal_folder, workflow=workflow)
+            pre_and_post_process(cif_filename=cif_file, cif_folder=cif_file_folder, cal_folder=cal_folder, workflow=workflow)
 
         cal_status = check_calculations_status(cal_folder=cal_folder)
         update_running_jobs_status(cal_status["running_folder_list"], workflow=workflow)
@@ -47,21 +46,17 @@ if __name__ == "__main__":
         ready_job_list = cal_status["prior_ready_folder_list"] + cal_status["ready_folder_list"]
         submit_jobs(ready_jobs=ready_job_list, workflow=workflow,max_jobs_in_queue=5)
         kill_error_jobs(error_jobs=cal_status["error_folder_list"], workflow=workflow)
+        
+        for cif_file in cif_file_list:
+            pre_and_post_process(cif_filename=cif_file, cif_folder=cif_file_folder, cal_folder=cal_folder, workflow=workflow)
+            
         cal_status = check_calculations_status(cal_folder=cal_folder)
         
-        
-        
-        
-        
-        to_be_cal_folders = []
-        for folder_name, folder_list in cal_status.items():
-            if folder_name != "done_folder_list":
-                to_be_cal_folders += folder_list
                 
         print("\n")
-            print(get_time_str())
-            pprint.pprint(cal_status)
-            time.sleep(60)
+        print(get_time_str())
+        pprint.pprint(cal_status)
+        
             
         with open("htc_job_status.dat", "w") as f:
                 f.write("\n{}:".format(get_time_str()))
@@ -70,10 +65,15 @@ if __name__ == "__main__":
                     for folder in folder_list:
                         f.write("\t{}\n".format(folder))
                         
+        to_be_cal_folders = []
+        for folder_name, folder_list in cal_status.items():
+            if folder_name != "done_folder_list":
+                to_be_cal_folders += folder_list
         if to_be_cal_folders == []:
             print("All calculations have finished --> Stop this program.")
             with open("htc_job_status.dat", "w") as f:
                 f.write("\n***All calculations have finished --> Stop this program.***")
             break
             
+        time.sleep(60)
 
