43d222691eeb601b08fbe5903a33a9d5f3ed7a8a
yang tong <bitsoal@gmail.com>
2026-04-07 18:22:40 +0800

    bugfix

--- HTC_lib/VASP/Job_Management/Check_and_update_calculation_status.py
+++ HTC_lib/VASP/Job_Management/Check_and_update_calculation_status.py
@@ -323,7 +323,7 @@ def check_calculations_status(cal_folder, workflow, mat_folder_name_list=None, c
                 continue
 
             # exclude anything under error_folder* 
-            if not any(part.startswith("error_folder") for part in subdir.parts):
+            if any(part.startswith("error_folder") for part in subdir.parts):
                 continue
 
             has_incar = (subdir / "INCAR").is_file()
